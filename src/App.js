e8e1d9f0abcfef7776b21ce4d8c74dbb3d91d95f
Lukas Tatarūnas <lukasttatarunas@gmail.com>
2019-06-27 00:09:05 +0300

    Finished the UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    LRT: []
+  }
+  
+  componentDidMount() {
+    fetch(`http://localhost:5500/api/v1/lrtlive`)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ LRT: data.tvprog.items })
+    })
+  }
+
+  render() {
+    return (
+       <div className='container'>
+        <div className='col-xs-12'>
+        <h1 className='tc'>LRT</h1>
+        {this.state.LRT.map((LRT, i) => (
+          <div key={ i } className='card'>
+            <div className='card-body bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+              <h6 className='card-title'>{ LRT.channel }</h6>
+              <h6 className='card-title'>{ `${ LRT.time_start } - ${ LRT.time_end }` }</h6>
+              <h1 className='card-title'>{ LRT.title }</h1>
+            </div>
+          </div>
+        ))}
+        </div>
+       </div>
+    )
+  }
 }
 
-export default App;
+export default App
